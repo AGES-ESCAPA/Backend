aefeda2f344ac2506b132716fef9f1879eeb2935
Lucas Goettert Lopes <lucas.goettert@edu.pucrs.br>
2026-09-08 21:44:16 -0300

    refactor: update endpoint mapping to use /api/v1 prefix

--- src/main/java/com/escapa/backend/adapters/controller/PublicCourseController.java
+++ src/main/java/com/escapa/backend/adapters/controller/PublicCourseController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/public/courses")
+@RequestMapping("/api/v1/public/courses")
 public class PublicCourseController {
 
     private final ListPublishedCoursesUseCase listPublishedCoursesUseCase;
